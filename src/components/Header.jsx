cf1f0fd9bfbdeb13b8b5c855038851ae60e09651
Ринат <brinat007@yandex.ru>
2020-06-22 22:15:10 +0300

    initial commit, page with some elements

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='header'>
+    <img src='https://gloria-project.eu/wp-content/uploads/2012/06/logo-256-transp.png' />
+  </header>
+}
+
+export default Header;
